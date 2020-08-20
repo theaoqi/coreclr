dotnet/runtime

commit 1e7552afb359d1fba2dc57d6f82f03d20db32dd2

Fix LTTng build for build environments with older liblttng-ust-dev  (#27273)
* Fix macro redefinition to use XplatEventLogger instead of simply writing FALSE

* Fix linker error

* undo newline changes

* Some changes to comment

* Move wrapper export from eventpipe.cpp to eventtrace.cpp

src/scripts/genLttngProvider.py
@@ -546,7 +546,10 @@ def generateLttngFiles(etwmanifest,eventprovider_directory, dryRun):
                 lttngimpl_file.write("#include \"" + lttngevntheadershortname + "\"\n\n")
 
                 lttngimpl_file.write("""#ifndef tracepoint_enabled
-#define tracepoint_enabled(provider, name) FALSE
+
+extern "C" bool XplatEventLoggerIsEnabled();
+
+#define tracepoint_enabled(provider, name) XplatEventLoggerIsEnabled()
 #define do_tracepoint tracepoint
 #endif
 

src/vm/eventtrace.cpp
@@ -7540,3 +7540,11 @@ bool EventPipeHelper::IsEnabled(DOTNET_TRACE_CONTEXT Context, UCHAR Level, ULONG
     return false;
 }
 #endif // FEATURE_PERFTRACING
+
+#if defined(FEATURE_PAL)  && defined(FEATURE_PERFTRACING)
+// This is a wrapper method for LTTng. See https://github.com/dotnet/coreclr/pull/27273 for details.
+extern "C" bool XplatEventLoggerIsEnabled()
+{
+    return XplatEventLogger::IsEventLoggingEnabled();
+}
+#endif // FEATURE_PAL && FEATURE_PERFTRACING

theaoqi/coreclr

commit a8756b74cd23c66a1d522b8f620d0a0502b85560

14282: revert Fix LTTng build for build environments with older liblttng-ust-dev
https://github.com/dotnet/coreclr/commit/1e7552afb359d1fba2dc57d6f82f03d20db32dd2 caused failure of crossbuild for mips64

Change-Id: I39567cf45766646bb90ef4668662cd541b671bb0

src/scripts/genLttngProvider.py
@@ -546,10 +546,7 @@ def generateLttngFiles(etwmanifest,eventprovider_directory, dryRun):
                 lttngimpl_file.write("#include \"" + lttngevntheadershortname + "\"\n\n")
 
                 lttngimpl_file.write("""#ifndef tracepoint_enabled
-
-extern "C" bool XplatEventLoggerIsEnabled();
-
-#define tracepoint_enabled(provider, name) XplatEventLoggerIsEnabled()
+#define tracepoint_enabled(provider, name) FALSE
 #define do_tracepoint tracepoint
 #endif
 

src/vm/eventtrace.cpp
@@ -7543,11 +7543,3 @@ bool EventPipeHelper::IsEnabled(DOTNET_TRACE_CONTEXT Context, UCHAR Level, ULONG
     return false;
 }
 #endif // FEATURE_PERFTRACING
-
-#if defined(FEATURE_PAL)  && defined(FEATURE_PERFTRACING)
-// This is a wrapper method for LTTng. See https://github.com/dotnet/coreclr/pull/27273 for details.
-extern "C" bool XplatEventLoggerIsEnabled()
-{
-    return XplatEventLogger::IsEventLoggingEnabled();
-}
-#endif // FEATURE_PAL && FEATURE_PERFTRACING